0532fb456e58325fc4b5165e4e3eae0707be2a6a
Luisa <luhcotmel@gmail.com>
2024-02-19 18:34:08 -0300

    Adicionar arquivos de projeto.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblHelloWorld = new Label();
+            btnClickThis = new Button();
+            SuspendLayout();
+            // 
+            // lblHelloWorld
+            // 
+            lblHelloWorld.AutoSize = true;
+            lblHelloWorld.Location = new Point(345, 259);
+            lblHelloWorld.Name = "lblHelloWorld";
+            lblHelloWorld.Size = new Size(45, 20);
+            lblHelloWorld.TabIndex = 1;
+            lblHelloWorld.Text = "Texto";
+            // 
+            // btnClickThis
+            // 
+            btnClickThis.Location = new Point(314, 215);
+            btnClickThis.Name = "btnClickThis";
+            btnClickThis.Size = new Size(94, 29);
+            btnClickThis.TabIndex = 0;
+            btnClickThis.Text = "Click This";
+            btnClickThis.UseVisualStyleBackColor = true;
+            btnClickThis.Click += btnClickThis_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1057, 594);
+            Controls.Add(lblHelloWorld);
+            Controls.Add(btnClickThis);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+
+        #endregion
+
+        private Label lblHelloWorld;
+        private Button btnClickThis;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickThis_Click_1(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World";
+        }
+    }
+}
